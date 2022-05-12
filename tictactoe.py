71a6f267bf53f15cd34d359c285f78ca9bfc2120
Manolo Medina <manomed@LAPTOP-1DOFACG0.localdomain>
2022-05-12 10:31:30 -0500

    Se modifica el archivo para cumplir con los estandares PEP8

--- tictactoe.py
+++ tictactoe.py
@@ -8,15 +8,15 @@ Exercises
 4. How could you create a computer player?
 """
 
-from turtle import *
+import turtle
 
 from freegames import line
 
 
 def grid():
     """Draw tic-tac-toe grid."""
-    pencolor("blue")
-    bgcolor("white")
+    turtle.pencolor("blue")
+    turtle.bgcolor("white")
     line(-67, 200, -67, -200)
     line(67, 200, 67, -200)
     line(-200, -67, 200, -67)
@@ -25,18 +25,18 @@ def grid():
 
 def drawx(x, y):
     """Draw X player."""
-    pencolor("green")
+    turtle.pencolor("green")
     line(x+30, y+30, x + 100, y + 100)
     line(x+30, y + 100, x + 100, y+30)
 
 
 def drawo(x, y):
     """Draw O player."""
-    pencolor("red")
-    up()
-    goto(x + 64, y + 20)
-    down()
-    circle(45)
+    turtle.pencolor("red")
+    turtle.up()
+    turtle.goto(x + 64, y + 20)
+    turtle.down()
+    turtle.circle(45)
 
 
 def floor(value):
@@ -47,15 +47,16 @@ def floor(value):
 state = {'player': 0}
 players = [drawx, drawo]
 # Arreglo para verificar si un espacio ya esta ocupado
-places_on_grid= [" "," "," "," "," "," "," "," "," "]
+places_on_grid = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def draw_symbol(x,y,player):
+
+def draw_symbol(x, y, player):
     """Draw either the x or the o accordingly"""
     draw = players[player]
     draw(x, y)
-    update()
+    turtle.update()
     # Cambia de 'x' a 'o'
-    state['player'] = not player  
+    state['player'] = not player
 
 
 def tap(x, y):
@@ -63,86 +64,86 @@ def tap(x, y):
     global places_on_grid
     x = floor(x)
     y = floor(y)
-    column=(x+205) 
-    row= (-y+205)
-    square= column +row*3
-    square=int(square)
+    column = (x + 205)
+    row = (-y + 205)
+    square = column + row*3
+    square = int(square)
     player = state['player']
-    
-    if square == 422:
-        if places_on_grid[0]==" ":
-            places_on_grid[0]=1
-            draw_symbol(x,y,player)
 
-        elif places_on_grid[0]!="":
+    if square == 422:
+        if places_on_grid[0] == " ":
+            places_on_grid[0] = 1
+            draw_symbol(x, y, player)
+
+        elif places_on_grid[0] != "":
             print("This space has already been used")
     elif square == 555:
-        if places_on_grid[1]==" ":
-            places_on_grid[1]=1
-            draw_symbol(x,y,player)
+        if places_on_grid[1] == " ":
+            places_on_grid[1] = 1
+            draw_symbol(x, y, player)
 
         else:
             print("This space has already been used")
     elif square == 688:
-        if places_on_grid[2]==" ":
-            places_on_grid[2]=1
-            draw_symbol(x,y,player)
+        if places_on_grid[2] == " ":
+            places_on_grid[2] = 1
+            draw_symbol(x, y, player)
 
         else:
             print("This space has already been used")
     elif square == 821:
-        if places_on_grid[3]==" ":
-            places_on_grid[3]=821
-            draw_symbol(x,y,player)
+        if places_on_grid[3] == " ":
+            places_on_grid[3] = 821
+            draw_symbol(x, y, player)
 
         else:
             print("This space has already been used")
 
     elif square == 954:
-        if places_on_grid[4]==" ":
-            places_on_grid[4]=1
-            draw_symbol(x,y,player)
+        if places_on_grid[4] == " ":
+            places_on_grid[4] = 1
+            draw_symbol(x, y, player)
 
         else:
             print("This space has already been used")
 
     elif square == 1087:
-        if places_on_grid[5]==" ":
-            places_on_grid[5]=1
-            draw_symbol(x,y,player)
+        if places_on_grid[5] == " ":
+            places_on_grid[5] = 1
+            draw_symbol(x, y, player)
 
         else:
             print("This space has already been used")
 
     elif square == 1220:
-        if places_on_grid[6]==" ":
-            places_on_grid[6]=1
-            draw_symbol(x,y,player)
+        if places_on_grid[6] == " ":
+            places_on_grid[6] = 1
+            draw_symbol(x, y, player)
 
         else:
             print("This space has already been used")
 
     elif square == 1353:
-        if places_on_grid[7]==" ":
-            places_on_grid[7]=1
-            draw_symbol(x,y,player)
+        if places_on_grid[7] == " ":
+            places_on_grid[7] = 1
+            draw_symbol(x, y, player)
 
         else:
             print("This space has already been used")
 
     elif square == 1486:
-        if places_on_grid[8]==" ":
-            places_on_grid[8]=1
-            draw_symbol(x,y,player)
+        if places_on_grid[8] == " ":
+            places_on_grid[8] = 1
+            draw_symbol(x, y, player)
 
         else:
             print("This space has already been used")
 
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
+turtle.setup(420, 420, 370, 0)
+turtle.hideturtle()
+turtle.tracer(False)
 grid()
-update()
-onscreenclick(tap)
-done()
+turtle.update()
+turtle.onscreenclick(tap)
+turtle.done()
